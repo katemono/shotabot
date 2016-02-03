89e28ed3769d87020694eda496cb90d144d1199d
normie <normie@normies-MacBook-Pro.local>
2016-02-03 01:33:24 -0600

    lots added still not enough comments or clean up

--- shotabot.rb
+++ shotabot.rb
@@ -1,23 +1,34 @@
 require './shotaclass'
 
-userinfo=[]
-File.open("config", "r") do |f|
-  f.each_line do |line|
-    userinfo.push line
-  end
+prepare
+
+bot = Discordrb::Bot.new $info["user"].chomp, $info["pass"].chomp
+
+bot.message(starting_with: "#{$info["prefix"]}threaddump") do |event|
+  drop_thread(event)
 end
 
-bot = Discordrb::Bot.new userinfo[0].chomp, userinfo[1].chomp
-
-bot.message(starting_with: "~coinflip") do |event|
-  event.respond coinflip
+bot.message(starting_with: "#{$info["prefix"]}staff") do |event|
+  returnstaff(event)
 end
 
-bot.message(starting_with: "~8ball") do |event|
-  event.respond z8ball
+bot.message(starting_with: "#{$info["prefix"]}gelbooru") do |event|
+  randomGelbooru(event)
 end
 
-bot.message(starting_with: "~massmention") do |event|
+bot.message(starting_with: "#{$info["prefix"]}danbooru") do |event|
+  randomDanbooru(event)
+end
+
+bot.message(starting_with: "#{$info["prefix"]}help") do |event|
+  help(event)
+end
+
+bot.message(starting_with: "#{$info["prefix"]}watchthread") do |event|
+  watchthread(event)
+end
+
+bot.message(starting_with: "#{$info["prefix"]}massmention") do |event|
   if event.author.permission?(:kick_members, event.server, event.channel)
     massmention(event)
   else
@@ -25,35 +36,15 @@ bot.message(starting_with: "~massmention") do |event|
   end
 end
 
-bot.message(starting_with: "~triggered") do |event|
-  event.respond("http://puu.sh/mSpsT/1cd32bc004.jpg")
-end
-
-bot.message(starting_with: "~masspm") do |event|
+bot.message(starting_with: "#{$info["prefix"]}masspm") do |event|
   if event.author.permission?(:kick_members, event.server, event.channel)
-    event.respond mass_pm(event.channel.users, event.text.sub('~masspm', ''))
+    event.respond mass_pm(event)
   else
     event.respond("You arent allowed to do that! >:c")
   end
 end
 
-bot.message(starting_with: "~threaddump") do |event|
-  (drop_thread(event.text.sub("~threaddump", ''))).each do |x| 
-    event.send_message(x)
-    sleep 1
-  end
-  event.respond "Done!"
-end
-
-bot.message(starting_with: "~staff") do |event|
-  returnstaff(event)
-end
-
-bot.message() do |event|
-  catchallevent(event)
-end
-
-bot.message(starting_with: "~mimic") do |event|
+bot.message(starting_with: "#{$info["prefix"]}mimic") do |event|
   if event.author.permission?(:kick_members, event.server, event.channel)
     mimic(event)
   else
@@ -61,40 +52,40 @@ bot.message(starting_with: "~mimic") do |event|
   end
 end
 
-bot.message(starting_with: "~gelbooru") do |event|
-  event.respond randomGelbooru(event.text.sub("~gelbooru",""));
-end
-
-bot.message(starting_with: "~rape") do |event|
+bot.message(starting_with: "#{$info["prefix"]}rape") do |event|
   event.respond "_Holds down and cums inside #{event.message.mentions[0].mention}_"
 end
 
-bot.message(starting_with: "~cat") do |event|
-  event.respond randomCat
+bot.message(starting_with: "#{$info["prefix"]}triggered") do |event|
+  event.respond("http://puu.sh/mSpsT/1cd32bc004.jpg")
 end
 
-bot.message(starting_with: "~danbooru") do |event|
-  event.respond randomDanbooru(event.text.sub("~gelbooru",""));
-end
-
-bot.message(starting_with: "~mrpython") do |event|
-  event.respond getrandompythons
-end
-
-bot.message(starting_with: "~retard") do |event|
+bot.message(starting_with: "#{$info["prefix"]}retard") do |event|
   event.respond "https://u.pomf.is/hfjsmj.gif"
 end
 
-bot.message(starting_with: "~doit") do |event|
+bot.message(starting_with: "#{$info["prefix"]}doit") do |event|
   event.respond "https://u.pomf.is/bgqhef.gif"
 end
 
-bot.message(starting_with: "~help") do |event|
-  help(event)
+bot.message(starting_with: "#{$info["prefix"]}cat") do |event|
+  event.respond randomCat
 end
 
-bot.message(starting_with: "~watchthread") do |event|
-  watchthread(event.text.sub("~watchthread", ''),event)
+bot.message(starting_with: "#{$info["prefix"]}mrpython") do |event|
+  event.respond getrandompythons
+end
+
+bot.message(starting_with: "#{$info["prefix"]}coinflip") do |event|
+  event.respond coinflip
+end
+
+bot.message(starting_with: "#{$info["prefix"]}8ball") do |event|
+  event.respond z8ball
+end
+
+bot.message() do |event|
+  catchallevent(event)
 end
 
 bot.run
--- shotaclass.rb
+++ shotaclass.rb
@@ -5,6 +5,12 @@ require "open-uri"
 
 $mimicked = []
 $watchedthreads = []
+$info={}
+
+def prepare
+  $info = JSON.parse(File.open("config", "r").read)
+end
+
 def help(event)
   if event.author.permission?(:kick_members, event.server, event.channel)
     event.author.pm("~mimic @mentionuser
@@ -31,7 +37,7 @@ def mimic(event)
   begin 
     id = event.message.mentions[0].id
     if $mimicked.include? id
-      $mimicked.delete(id)
+      $mimicked-=[id]
     else
       $mimicked.push(id)
     end
@@ -44,8 +50,8 @@ end
 def returnstaff(event)
   message = "Staff is:\n"
   for user in event.channel.users do
-   message+="`**ADMIN** " if user.permission?(:manage_server, event.server, event.channel)
-   message+=user.name+"`\n" if user.permission?(:kick_members, event.server, event.channel) && user.bot? != true
+   message+="*ADMIN* " if user.permission?(:manage_server, event.server, event.channel)
+   message+=user.name+"\n" if user.permission?(:kick_members, event.server, event.channel) && user.bot? != true
   end
   event.send_message(message);
 end
@@ -111,37 +117,21 @@ def scrapthread(url)
   return files
 end
 
-def watchthread(url, event)
+def watchthread(event)
   thread = {}
+  url = event.text.sub("#{$info["prefix"]}watchthread", '')
   thread["thread"]=scrapthread(url)
   thread["time"] = Time.now
   thread["url"]  = url
   thread["author"] = event.author
-  message = ""
-  messages = []
-  for x in thread["thread"] do
-    if message.length <= 1000 then
-      message+=x+" " 
-    else
-      messages.push(message)
-      message=x+" "
-    end
-  end
-  messages.push(message) unless messages.include? message
-  messages.each do|x| 
-    event.author.pm(x)
-    sleep 0.1
-  end
+  handlemessages(thread["thread"],event.author,pm = true)
   $watchedthreads.push thread
 end
 
-
-def checkup(author,thread,url)
-  newthread = scrapthread(url)
-  threadx = newthread - thread
+def handlemessages(items,event,pm = false)
   message = ""
   messages = []
-  for x in threadx do
+  for x in items do
     if message.length <= 1000 then
       message+=x+" " 
     else
@@ -149,72 +139,39 @@ def checkup(author,thread,url)
       message=x+" "
     end
   end
-  messages.push(message) unless messages.include? message
+  messages.push(message) unless messages.include? message or message == ""
   messages.each do|x| 
-    author.pm(x)
+    if pm == true
+      event.pm x
+    else
+      event.send_message x
+    end
     sleep 0.1
   end
+end
+
+def checkup(x)
+  author = x["author"]
+  thread = x["thread"]
+  url = x["url"]
+  newthread = scrapthread(url)
+  threadx = newthread - thread
+  handlemessages(threadx,author,pm = true)
   return newthread
 end
   
-def drop_thread(url)
-  agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
-
-  url = url.chomp.lstrip
-  html_doc = Nokogiri::HTML(agent.get(url).body)
-  files=[]
-
-  if /:\/\/boards\.4chan\.org/.match(url) do
-    (html_doc.css('div.file')).each{ |ito| files.push "https:#{ito.css("a")[0]['href']}"}
-  end
-
-  elsif /:\/\/7chan\.org/.match(url) do
-    (html_doc.css('p.file_size')).each{ |ito| files.push "#{ito.css("a")[0]['href']}"}
-  end
-
-  elsif /:\/\/boards\.420chan\.org/.match(url) do
-    (html_doc.css('span.filesize')).each{ |ito| files.push "#{ito.css("a")[0]['href']}"}
-  end
-
-  else 
-    (html_doc.css('p.fileinfo')).each{ |ito| files.push "#{ito.css("a")[0]['href']}"}
-  end
-  
-  retarr = []
-  prefix = (url.split '/')[0..-4].join '/'
-  if prefix.include? "420chan.org"
-    for x in files
-      if x.include? "http"
-        retarr.push x
-      else
-        retarr.push("#{prefix}#{x}")
-      end
-    end
-    files = retarr
-  end
-  
-  messages = []
-  message=""
-  
-  #split into a few messages to avoid rate limiting shit
-  for x in files do
-    if message.length <= 1000 then
-      message+=x+" " 
-    else
-      messages.push(message)
-      message=x+" "
-    end
-  end
-  messages.push(message) unless messages.include? message
-  return messages
+def drop_thread(event)
+  files=scrapthread(event.text.sub("#{$info["prefix"]}threaddump", ''))
+  handlemessages(files,event)
 end
-
 
 def getrandompythons
   return ((URI.parse('http://tomokochan.me/pythons.txt')).read).split.sample
 end
 
-def mass_pm(users, text)
+def mass_pm(event)
+  users = event.channel.users 
+  text = event.text.sub("#{$info["prefix"]}masspm", '')
   for user in users do
     #spit out nasty error if you pm bot
     user.pm(text) unless user.bot?
@@ -239,18 +196,20 @@ def massmention(event)
     event.send_message(x)
     sleep 0.1
   end
-  event.respond event.text.sub("~massmention","")
+  event.respond event.text.sub("#{$info["prefix"]}massmention","")
 end
 
-def randomGelbooru(tag)
+def randomGelbooru(event)
   #wont work often due to anonymous user issues
+  tag = event.text.sub("#{$info["prefix"]}gelbooru","")
   url="http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=20&id=2000000&json=1&tags="+((tag.split).join '+')
-  return (JSON.parse(((URI.parse(url)).read)).sample["file_url"]).sub('\/','/')
+  event.respond (JSON.parse(((URI.parse(url)).read)).sample["file_url"]).sub('\/','/')
 end
 
-def randomDanbooru(tag)
+def randomDanbooru(event)
+  tag = event.text.sub("#{$info["prefix"]}gelbooru","")
   url="http://danbooru.donmai.us/posts.json?tags="+((tag.split).join '+~')
-  return "http://danbooru.donmai.us"+(JSON.parse(((URI.parse(url)).read)).sample["file_url"])
+  event.respond "http://danbooru.donmai.us"+(JSON.parse(((URI.parse(url)).read)).sample["file_url"])
 end
 
 def randomCat
@@ -262,7 +221,7 @@ def catchallevent(event)
   if $watchedthreads.length > 0
     for x in $watchedthreads
       if (Time.now - x["time"]) > 600
-        x["thread"] = checkup(x["author"],x["thread"], x["url"])
+        x["thread"] = checkup(x)["thread"]
         x["time"] = Time.now
       end
     end
